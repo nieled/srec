e978c2473a8d166435a0ebdab7d090988c3b729f
Eduardo Calle <nieled@riseup.net>
2016-11-19 23:03:45 -0500

    Add: all client interface

---
+++ client/main.js
@@ -0,0 +1,6 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import { Ratings } from '../imports/api/ratings';
+import { Users } from '../imports/api/users';
+
+import { name as Srec } from '../imports/ui/components/srec/srec';
